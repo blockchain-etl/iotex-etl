035ef237fa6796e0c02bd0b7db7c092185d61de0
Worawat Wijarn <worawat.wijarn@gmail.com>
2020-07-25 20:19:53 +0700

    Add tests for frequently used actions

--- tests/iotexetl/jobs/test_export_blocks_job.py
+++ tests/iotexetl/jobs/test_export_blocks_job.py
@@ -40,6 +40,14 @@ def read_resource(resource_group, file_name):
 @pytest.mark.parametrize("start_block, end_block, resource_group ,provider_type", [
     (3290498, 3290498, 'blocks_with_actions', 'mock'),
     skip_if_slow_tests_disabled([3290498, 3290498, 'blocks_with_actions', 'online']),
+    (5728441, 5728441, 'blocks_with_executions', 'mock'),
+    skip_if_slow_tests_disabled([5728441, 5728441, 'blocks_with_executions', 'online']),
+    (5914081, 5914081, 'blocks_with_put_poll_result', 'mock'),
+    skip_if_slow_tests_disabled([5914081, 5914081, 'blocks_with_put_poll_result', 'online']),
+    (5908046, 5908046, 'blocks_with_claim_from_rewarding_fund', 'mock'),
+    skip_if_slow_tests_disabled([5908046, 5908046, 'blocks_with_claim_from_rewarding_fund', 'online']),
+    (5911020, 5911020, 'blocks_with_stake_create', 'mock'),
+    skip_if_slow_tests_disabled([5911020, 5911020, 'blocks_with_stake_create', 'online']),
 ])
 def test_export_blocks_job(tmpdir, start_block, end_block, resource_group, provider_type):
     job = ExportBlocksJob(
