c5097055bdaef30fbf0e11d101415602d012bcc1
medvedev1088 <medvedev1088@gmail.com>
2020-09-28 23:49:25 +0700

    Add transaction_logs_have_latest.sql verification

--- airflow/dags/iotexetl_airflow/build_verify_streaming_dag.py
+++ airflow/dags/iotexetl_airflow/build_verify_streaming_dag.py
@@ -68,6 +68,7 @@ def build_verify_streaming_dag(
     add_verify_tasks('blocks_have_latest')
     add_verify_tasks('actions_have_latest')
     add_verify_tasks('logs_have_latest')
+    add_verify_tasks('transaction_logs_have_latest')
     add_verify_tasks('blocks_count')
     add_verify_tasks('actions_count')
     add_verify_tasks('errors_count')
